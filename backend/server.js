d6598c6fbb6cbbbe02cb41c6560df19285d7c5f5
adama diouf <adamadiouf2017@gmail.com>
2024-10-14 12:08:29 -0500

    add file

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
 import express from 'express'
 import path from 'path'
 import contactRoute from './routes/contactRoute.js'
-
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import { fileURLToPath } from 'url'
+
+// Setup environment variables
 dotenv.config()
+
+// Connect to the database
 connectDB()
+
+// Get the current directory name
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Set up the Express application
 const port = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// API route
 app.use('/api/form/contact', contactRoute)
 
+// Serve static files and define fallback route for React app
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend/build')))
   app.get('*', (req, res) =>
@@ -23,4 +35,6 @@ if (process.env.NODE_ENV === 'production') {
     res.send('API is running....')
   })
 }
-app.listen(port, () => console.log(`The server running at port ${port}`))
+
+// Start the server
+app.listen(port, () => console.log(`The server is running at port ${port}`))
